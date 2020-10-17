c43a7d32800ea7e5b958e4fe33d885e2f8ae4414
Erin <erin3scully@icloud.com>
2020-10-17 12:34:51 -0600

    Building HomeViewController UI

--- !SPOTIFY!/Controllers/HomeViewController.swift
+++ !SPOTIFY!/Controllers/HomeViewController.swift
@@ -1,11 +1,13 @@
-
 import UIKit
 
 class HomeViewController: UIViewController {
 
+    var categories: [Category]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
+        categories = CategoryService.shared.categories
     }
 
 
---
+++ !SPOTIFY!/Models/CategoryCell.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
