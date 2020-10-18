e5b9e6dff1cce2fe321bfd7742d68448b7d02c6d
Erin <erin3scully@icloud.com>
2020-10-18 17:51:25 -0600

    Data seeding AlbumViewController successful

--- !SPOTIFY!/Models/Service/AlbumService.swift
+++ !SPOTIFY!/Models/Service/AlbumService.swift
@@ -3,4 +3,12 @@ import Foundation
 
 class AlbumService {
     static let shared = AlbumService()
-}
+   
+    private init(){}
+    
+        func getAlbum(from category: Category)-> [Album] {
+            let album = category.albums
+            return album
+        }
+    }
+   
