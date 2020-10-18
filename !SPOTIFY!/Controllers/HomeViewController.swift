ad618e95f4f99fe5ad700c58e200257c0f82df4b
Erin <erin3scully@icloud.com>
2020-10-18 15:54:43 -0600

    HomeViewController table view data source method

--- !SPOTIFY!/Controllers/HomeViewController.swift
+++ !SPOTIFY!/Controllers/HomeViewController.swift
@@ -2,14 +2,42 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    
     var categories: [Category]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
        
         categories = CategoryService.shared.categories
+   
     }
 
 
+}
+
+
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as! CategoryCell
+        let category = categories[indexPath.row]
+        cell.updateCategoryCell(with: category, indexPath: indexPath.row)
+        return cell
+    }
+    
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
 
