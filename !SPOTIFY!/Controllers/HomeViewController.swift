eab976a50233b7d654911cfad8b00e960bf2de18
Erin <erin3scully@icloud.com>
2020-10-18 16:24:18 -0600

    Update CategoryCell class model and set the collection view tag property equal to the table cell position
    Implement UICollectionViewDataSource methods in HomeViewController

--- !SPOTIFY!/Controllers/HomeViewController.swift
+++ !SPOTIFY!/Controllers/HomeViewController.swift
@@ -3,13 +3,13 @@ import UIKit
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-
     
+   
     var categories: [Category]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //Linked the view controller as the collection view's data source in main.storyboard
         tableView.dataSource = self
         tableView.delegate = self
        
@@ -30,7 +30,7 @@ extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as! CategoryCell
         let category = categories[indexPath.row]
-        cell.updateCategoryCell(with: category, indexPath: indexPath.row)
+        cell.updateCategoryCell(with: category, index: indexPath.row)
         return cell
     }
     
@@ -38,6 +38,29 @@ extension HomeViewController: UITableViewDataSource {
 }
 
 extension HomeViewController: UITableViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //The number of items to display in the collection varies depending on the particular CATEGORY the section is in. That is why the category cell method assigns the collection view's TAG (aka POSITION)  property equal to the POSITION of the table view cell
+        let categoryIndex = collectionView.tag
+        let category = categories[categoryIndex]
+        //the collection view should return as many albums as there are in a given category section
+        return category.albums.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCell
+        // Copy and pasted "category index" and  "category" properties from the numberOfItemsInSection method above in order to get the CATEGORY position because the album cell will display different content based on the category its under
+        let categoryIndex = collectionView.tag
+        let category = categories[categoryIndex]
+        //Use the indexpath.row position to retrieve the correct album cell for a given category
+        let album = category.albums[indexPath.row]
+        cell.updateAlbumCell(with: album)
+        return cell
+    }
+    
     
 }
 
--- !SPOTIFY!/Views/CategoryCell.swift
+++ !SPOTIFY!/Views/CategoryCell.swift
@@ -8,11 +8,13 @@ class CategoryCell: UITableViewCell {
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    func updateCategoryCell(with category: Category, indexPath: Int){
+    func updateCategoryCell(with category: Category, index: Int){
         titleLabel.text = category.title
         subtitleLabel.text = category.subtitle
+        //Assign the collection view tag property equal to the table view cell's indexpath.row property
+        collectionView.tag = index
+        collectionView.reloadData()
         
-        //will address the indexpath property later on...
     }
     
 }
