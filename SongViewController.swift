b3798a743d6bef9e7ffe66c7521a5e1dfed16869
Erin <erin3scully@icloud.com>
2020-10-18 22:18:06 -0600

    Rough draft of SongViewController and linked outlets

--- SongViewController.swift
+++ SongViewController.swift
@@ -4,7 +4,39 @@ import UIKit
 
 class SongViewController: UIViewController {
     
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var albumTitleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var songSlider: UISlider!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var endTimeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func likeButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func songSliderChanged(_ sender: UISlider) {
+    }
+    
+    
+    @IBAction func pauseButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func previousButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func dropdownArrowPressed(_ sender: UIButton) {
+    }
+    
 }
