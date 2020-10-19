f1a7c107d0fe4359cd618d88f85b10ce74b0e187
Erin <erin3scully@icloud.com>
2020-10-18 19:31:32 -0600

    Implemented follow/unfollow functionality in Album View Controller
    Button text reads Follow if user isn't following yet; and reads Following if they already are upon View did load. If the user  opts to change the value of follow/unfollow by pressing the button then the button will change accordingly and the number of followers in the description label count will increase or decrease by 1.

--- AlbumViewController.swift
+++ AlbumViewController.swift
@@ -14,6 +14,7 @@ class AlbumViewController: UIViewController {
     var songs: [Song]!
     var album: Album!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,12 +27,18 @@ class AlbumViewController: UIViewController {
         albumTitleLabel.text = album.name
         descriptionLabel.text = "\(album.followers) followers by \(album.artist)"
         
-        
+        if UserService.shared.isFollowingAlbum(album: album){
+            followButton.setTitle("Following", for: .normal)
+            followButton.layer.borderColor = #colorLiteral(red: 0.1627579331, green: 0.6996970177, blue: 0.2955926955, alpha: 1).cgColor
+        } else {
+            followButton.setTitle("Follow", for: .normal)
+            followButton.layer.borderColor = UIColor.white.cgColor
+        }
         
    //Change button appearance
         shuffleButton.layer.cornerRadius = 10.0
         followButton.layer.cornerRadius = 5.0
-        followButton.layer.borderWidth = 1.0
+        followButton.layer.borderWidth = 2.0
         followButton.layer.borderColor = UIColor.white.cgColor
         
         
@@ -40,6 +47,20 @@ class AlbumViewController: UIViewController {
     
 
     @IBAction func followButtonPressed(_ sender: UIButton) {
+      //Check if the user is already following the album:
+        if UserService.shared.isFollowingAlbum(album: album) {
+        //if YES and they tap the button then UNFOLLOW the album and change the button back to "Follow" with a white border:
+            UserService.shared.unfollowAlbum(album: album)
+            followButton.setTitle("Follow", for: .normal)
+            followButton.layer.borderColor = UIColor.white.cgColor
+        }
+        else {
+        //if NO, and they tap the button then FOLLOW the album:
+            UserService.shared.followAlbum(album: album)
+            followButton.setTitle("Following", for: .normal)
+            followButton.layer.borderColor = #colorLiteral(red: 0.1627579331, green: 0.6996970177, blue: 0.2955926955, alpha: 1).cgColor
+        }
+        descriptionLabel.text = "\(album.followers) followers by \(album.artist)"
     }
     
     @IBAction func shuffleButtonPressed(_ sender: UIButton) {
